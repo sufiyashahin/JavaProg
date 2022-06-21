16b34a200626a54e58dd8a97711d156146866e3f
PratikDive <107669527+PratikDive@users.noreply.github.com>
2022-06-21 13:21:17 +0530

    Add files via upload

---
+++ Assingment/PatternN.java
@@ -0,0 +1,16 @@
+package PratikJava;
+
+public class PatternN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int i = 1;
+	int j = 5;
+		
+		for ( i = 1, j = 5; i<=5 ; i++, j--) {
+	
+			System.out.println(i +""+j);
+		}
+}
+}
